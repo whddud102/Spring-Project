2d47646353405b061828afd8a9b356b9501c794a
Jongyoung <whddud@naver.com>
2020-03-17 23:44:01 +0900

    검색 조건을 처리하기 위해 Criteria 클래스를 확장

--- ex02/src/main/java/org/zerock/domain/Criteria.java
+++ ex02/src/main/java/org/zerock/domain/Criteria.java
@@ -11,6 +11,8 @@ public class Criteria {
 	private int pageNum;	// 페이지의 번호
 	private int amount;		// 필요한 게시글의 수
 	
+	private String type;	// 검색 조건
+	private String keyWorkd; // 검색 조건 키워드 
 	
 	public Criteria() {
 		// 기본 생성자로 페이지 번호 = 1, 게시글 수 = 10으로 지정
